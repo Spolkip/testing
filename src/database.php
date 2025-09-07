0522dbee8c28c4410ba79f53d921e9689f126dd9
Spolkip <79760598+Spolkip@users.noreply.github.com>
2025-09-07 21:21:10 +0300

    a

--- src/database.php
+++ src/database.php
@@ -1,11 +1,17 @@
 <?php 
-$host = "localhost";
-$port = 3306; // default XAMPP MySQL port
-$dbname = "texnologies_diadiktiou_db";
-$db_username = "root";
-$db_password = ""; // XAMPP default is empty
+    $host = "localhost";
+    $port = 8889;
+    $dbname = "texnologies_diadiktiou_db";
+    $db_username = "root";
+    $db_password = "root";
+    $socket = '/Applications/MAMP/tmp/mysql/mysql.sock';
 
-    $conn = mysqli_connect($host, $db_username, $db_password, $dbname, $port);
+    $conn = mysqli_connect($host, 
+                        $db_username, 
+                        $db_password, 
+                        $dbname, 
+                        $port, 
+                        $socket);
 
     if (mysqli_connect_errno()) {
         die("Connection error: " . mysqli_connect_error());
